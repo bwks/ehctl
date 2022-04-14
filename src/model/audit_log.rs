467dc34b761bdb7d9c9b9eb8b64f8b84ac8490f3
Brad Searle <bradleysearle@gmail.com>
2022-04-14 21:04:38 +1000

    auditlog

--- src/model/audit_log.rs
+++ src/model/audit_log.rs
@@ -4,16 +4,15 @@ use serde::Deserialize;
 
 use crate::deserialize::null_to_default;
 
-#[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Deserialize)]
+#[serde(default)]
 pub struct AuditLogs {
     pub audit_logs: Vec<AuditLog>,
 }
 
-#[allow(dead_code)]
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Deserialize)]
+#[serde(default)]
 pub struct AuditLog {
-    #[serde(default)]
     pub body: HashMap<String, serde_json::Value>,
     #[serde(deserialize_with = "null_to_default")]
     pub id: i64,
