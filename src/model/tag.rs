686cc7e735ba77cede67362f4707f9ea2a3a1ceb
Brad Searle <bradleysearle@gmail.com>
2022-04-15 13:17:33 +1000

    tags

--- src/model/tag.rs
+++ src/model/tag.rs
@@ -3,12 +3,13 @@ use tabled::Tabled;
 
 use crate::deserialize::null_to_default;
 
-#[derive(Deserialize)]
+#[derive(Default, Deserialize)]
+#[serde(default)]
 pub struct Tags {
     pub tags: Vec<Tag>,
 }
 
-#[derive(Tabled, Deserialize)]
+#[derive(Tabled, Default, Deserialize)]
 #[serde(default)]
 pub struct Tag {
     #[serde(deserialize_with = "null_to_default")]
@@ -17,14 +18,4 @@ pub struct Tag {
     pub mod_time: i64,
     #[serde(deserialize_with = "null_to_default")]
     pub name: String,
-}
-
-impl Default for Tag {
-    fn default() -> Self {
-        Self {
-            id: 0,
-            mod_time: 0,
-            name: "".to_string(),
-        }
-    }
 }
